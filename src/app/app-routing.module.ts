65f9291ed1a4f583145aa095c9fbf904baa6244e
govrdhn <ommigovardhan@gmail.com>
2024-11-26 19:02:20 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TaskComponent } from './task/task.component';
+import { AuthComponent } from './auth/auth.component';
+import { TasklistComponent } from './tasklist/tasklist.component';
+import { NewtasktableComponent } from './newtasktable/newtasktable.component';
+import { UpdatetableComponent } from './updatetable/updatetable.component';
+import { LoadspinnerComponent } from './loadspinner/loadspinner.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'task', component: TaskComponent},
+  { path: 'auth', component: AuthComponent},
+  { path: 'tasklist', component: TasklistComponent},
+  { path: 'newtasktable', component: NewtasktableComponent},
+  { path: 'update', component: UpdatetableComponent},
+  { path: 'loadspinner', component: LoadspinnerComponent},
+  { path: '**', component: LoginComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/supabase.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient, User, } from '@supabase/supabase-js';
+import { from, Observable } from 'rxjs';
+
+
+export interface Profile {
+  name: string
+  email: string
+  password: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient('', '');
+  }
+
+
+
+  async register(email: string, password: string) {
+    console.log(this.supabase)
+    let res = await this.supabase.auth.signUp({ "email": email, "password": password });
+    return res;
+  }
+
+
+
+  async login(email: string, password: string) {
+    let ress = await this.supabase.auth.signInWithPassword({ email, password });
+    console.log(ress)
+    return ress;
+  }
+
+  async submittask(name: string, desc: string, duetime: number, priority: number, user: string | null) {
+    const { error } = await this.supabase
+      .from('tasks')
+      .insert({ name: name, description: desc, duetime: duetime, priority: priority, userid: user })
+    if (error) {
+      console.log('error occured!')
+      return 'err';
+    }
+    else return 'noerror';
+  }
+
+  fetchtask(): Observable<any> {
+    const ress = this.supabase
+      .from('tasks')
+      .select();
+
+    return from(ress);
+  }
+  deletetask(name: string) {
+    const res = this.supabase
+      .from('tasks')
+      .delete()
+      .eq('taskid', name)
+    return from(res);
+  }
+  updatetask(name: string, desc: string, duetime: any, priority: number, taskid: string) {
+    const result = this.supabase
+      .from('tasks')
+      .update({ name: name, description: desc, duetime: duetime, priority: priority })
+      .eq('taskid', taskid)
+    console.log(result)
+    return from(result);
+  }
+
+  fetchwithid(taskid: string) {
+    const ress = this.supabase
+      .from('tasks')
+      .select()
+      .eq('taskid', taskid);
+
+    return from(ress);
+
+  }
+}
+
+
+
+
+
+/*async register( name:string, email: string, password: string) {
+      const { data, error } = await this.supabase
+      .from('users')
+      .insert([{name, email,password}]);
+      if(error){
+        console.log(error)
+        throw error;
+      }
+      console.log(data)
+      return data;
+    }
+ */
